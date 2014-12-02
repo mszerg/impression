60b5dae1f46d72c50e7d4e1d7fd1080c804bff5d
mszerg <mszerg@gmail.com>
2014-12-02 23:24:41 +0200

    Слайдера

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,12 +4,56 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <div class="container">
+        <div class="row otstup">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Маркеры слайдов -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+                    <!-- Содержимое слайдов -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="images/slider/1.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h3>Россия (Москва)</h3>
+                                <p>Где это видано, чтобы люди в Москве без прописки проживали.</p>
+                            </div>
+                        </div>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+                        <div class="item">
+                            <img src="images/slider/2.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h3>Осень</h3>
+                                <p>Осень — это вторая весна, когда каждый лист — цветок.</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="images/slider/3.jpg" alt="...">
+                            <div class="carousel-caption">
+                                <h3>Дождь</h3>
+                                <p>Осень опять идут дожди...</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
+                </div>
+
+            </div>
+        </div>
     </div>
 
     <div class="body-content">
