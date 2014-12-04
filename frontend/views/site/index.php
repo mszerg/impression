7123d08553be5cfdf34a2d38e32da29dd44542e2
mszerg <mszerg@gmail.com>
2014-12-04 21:47:03 +0200

    Slider

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,57 +4,87 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="container">
-        <div class="row otstup">
-            <div class="col-md-2"></div>
-            <div class="col-md-8">
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                    <!-- Маркеры слайдов -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                    </ol>
 
-                    <!-- Содержимое слайдов -->
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="images/slider/1.jpg" alt="...">
-                            <div class="carousel-caption">
-                                <h3>Россия (Москва)</h3>
-                                <p>Где это видано, чтобы люди в Москве без прописки проживали.</p>
-                            </div>
-                        </div>
 
-                        <div class="item">
-                            <img src="images/slider/2.jpg" alt="...">
-                            <div class="carousel-caption">
-                                <h3>Осень</h3>
-                                <p>Осень — это вторая весна, когда каждый лист — цветок.</p>
-                            </div>
-                        </div>
+    <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
 
-                        <div class="item">
-                            <img src="images/slider/3.jpg" alt="...">
-                            <div class="carousel-caption">
-                                <h3>Дождь</h3>
-                                <p>Осень опять идут дожди...</p>
-                            </div>
-                        </div>
-                    </div>
+        <!-- Carousel indicators -->
 
-                    <!-- Controls -->
-                    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                    </a>
-                    <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
+        <ol class="carousel-indicators">
+
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+
+        </ol>
+
+        <!-- Carousel items -->
+
+        <div class="carousel-inner">
+
+            <div class="item active">
+
+                <h2>Slide 1</h2>
+<center><img src="http://cs10426.vk.me/u72091913/a_4d5b93e5.jpg"></center>
+                <div class="carousel-caption">
+
+                    <h3>First slide label</h3>
+
+                    <p>Lorem ipsum dolor sit amet consectetur…</p>
+
                 </div>
 
             </div>
+
+            <div class="item">
+
+                <h2>Slide 2</h2>
+
+                <div class="carousel-caption">
+
+                    <h3>Second slide label</h3>
+
+                    <p>Aliquam sit amet gravida nibh, facilisis gravida…</p>
+
+                </div>
+
+            </div>
+
+            <div class="item">
+
+                <h2>Slide 3</h2>
+
+                <div class="carousel-caption">
+
+                    <h3>Third slide label</h3>
+
+                    <p>Praesent commodo cursus magna vel…</p>
+
+                </div>
+
+            </div>
+
         </div>
+
+        <!-- Carousel nav -->
+
+        <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+
+            <span class="glyphicon glyphicon-chevron-left"></span>
+
+        </a>
+
+        <a class="carousel-control right" href="#myCarousel" data-slide="next">
+
+            <span class="glyphicon glyphicon-chevron-right"></span>
+
+        </a>
+
     </div>
+
+
 
     <div class="body-content">
 
